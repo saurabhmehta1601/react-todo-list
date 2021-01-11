61422e1f2584b7cd59e1baa1a2939bff39bba7ec
saurabhmehta1601 <saurabhmehta1601@gmail.com>
2021-01-11 10:47:48 +0530

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from to do input</h1>
+      </div>
+    );
+  }
+}
